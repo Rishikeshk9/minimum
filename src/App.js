0a189489ab77e7e94ea2f9e08b4554e3dda0e67c
OrionROcket <orion@pariltech.com>
2024-05-13 02:44:11 +0400

    Fixed Glitch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
-
 const suits = ['♦', '♥', '♠', '♣'];
 const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
 const generateDeck = () => {
   let deck = [];
   for (let suit of suits) {
@@ -12,15 +10,13 @@ const generateDeck = () => {
   }
   return deck.sort(() => Math.random() - 0.5);
 };
-
 const reloadPage = () => {
   window.location.reload();
 };
-
-const Card = ({ card, onClick, highlightType }) => (
+const Card = ({ card, onClick, highlightType, className }) => (
   <div
     onClick={() => onClick(card)}
-    className='  m-3 p-6 px-7 bg-gray-200 rounded-md flex flex-col text-center hover:shadow-md cursor-pointer hover:-translate-y-2 transition-all duration-100'
+    className={` ${className} text-black  m-3 p-6 px-7 bg-gray-200 rounded-md flex flex-col text-center hover:shadow-md cursor-pointer hover:-translate-y-2 transition-all duration-100`}
     style={{
       backgroundColor: highlightType,
     }}
@@ -28,162 +24,216 @@ const Card = ({ card, onClick, highlightType }) => (
     {card.value}{' '}
     <p
       className={`${
-        card.suit === '♥' || card.suit === '♦' ? 'text-red-600' : ''
+        card.suit === '♥' || card.suit === ':diamonds:' ? 'text-red-600' : ''
       }`}
     >
       {card.suit}
     </p>
   </div>
 );
-
 function App() {
   const [deck, setDeck] = useState(generateDeck());
   const [players, setPlayers] = useState([]);
   const [activePlayerHand, setActivePlayerHand] = useState([]);
   const [highlightedPairs, setHighlightedPairs] = useState([]);
   const [highlightedSequences, setHighlightedSequences] = useState([]);
-
+  const [selectedCard, setSelectedCard] = useState(null);
   useEffect(() => {
     const [player1, player2, player3] = dealCards(deck);
     setPlayers([player1, player2, player3]);
     setActivePlayerHand(player1);
   }, [deck]);
-
   const dealCards = (deck) => {
     return [deck.slice(0, 7), deck.slice(7, 14), deck.slice(14, 21)];
   };
-
   const findSequences = (hand, selectedCard) => {
-    console.log('Original hand:', hand.slice());
-    const sortedHand = hand.slice().sort((a, b) => a.value - b.value);
-    console.log('Sorted hand:', sortedHand);
-    let sequences = [];
-    let currentSequence = [sortedHand[0]];
-
-    for (let i = 1; i < sortedHand.length; i++) {
-      const lastCard = currentSequence[currentSequence.length - 1];
-      const currentCard = sortedHand[i];
-      if (currentCard.value === lastCard.value + 1) {
-        currentSequence.push(currentCard);
+    // Sort the hand by value
+    const uniqueHand = hand.filter(
+      (card, index) => hand.findIndex((c) => c.value === card.value) === index
+    );
+    const sortedHand = uniqueHand.sort((a, b) => a.value - b.value);
+    let longestSequence = [];
+    let currentSequence = [];
+    // Iterate through the sortedHand
+    for (let i = 0; i < sortedHand.length; i++) {
+      // If the current card is part of a sequence
+      if (i === 0 || sortedHand[i].value === sortedHand[i - 1].value + 1) {
+        currentSequence.push(sortedHand[i]);
       } else {
-        if (
-          currentSequence.length >= 3 &&
-          currentSequence.includes(selectedCard)
-        ) {
-          sequences.push([...currentSequence]);
+        // Check if the current sequence is longer than the longest one found so far
+        if (currentSequence.length > longestSequence.length) {
+          longestSequence = [...currentSequence];
         }
-        currentSequence = [currentCard];
+        // Start a new sequence
+        currentSequence = [sortedHand[i]];
       }
     }
-
-    if (currentSequence.length >= 3 && currentSequence.includes(selectedCard)) {
-      sequences.push([...currentSequence]);
+    // Check if the last sequence is longer than the longest one found so far
+    if (currentSequence.length > longestSequence.length) {
+      longestSequence = [...currentSequence];
     }
-
-    return sequences.flat();
+    // Check if the longest sequence includes the selected card and has a length of at least 3
+    const includes1 = longestSequence.reduce(
+      (acc, card) => acc || card.value === selectedCard.value,
+      false
+    );
+    const includes2 = currentSequence.reduce(
+      (acc, card) => acc || card.value === selectedCard.value,
+      false
+    );
+    if (includes1 && longestSequence.length >= 3) {
+      console.log(includes1, includes2);
+      return longestSequence;
+    }
+    if (includes2 && currentSequence.length >= 3) {
+      console.log(includes1, includes2);
+      return currentSequence;
+    }
+    return [];
   };
-
   const handleCardClick = (selectedCard) => {
+    setSelectedCard(selectedCard);
     const sameValueCards = activePlayerHand.filter(
       (card) => card.value === selectedCard.value
     );
     let possiblePairs = [],
       possibleSequences = [];
-
     if (sameValueCards.length >= 2) {
       possiblePairs = [...sameValueCards];
     } else {
       possiblePairs = [];
     }
-
     const sequences = findSequences(activePlayerHand, selectedCard);
     if (sequences.length > 0) {
       possibleSequences = [...sequences];
     }
-
     setHighlightedPairs(possiblePairs);
     setHighlightedSequences(possibleSequences);
   };
-
   const executeMove = (moveType) => {
     let selectedCards =
       moveType === 'pair' ? highlightedPairs : highlightedSequences;
-    const remainingCards = activePlayerHand.filter(
-      (card) => !selectedCards.includes(card)
-    );
+    const remainingCards = activePlayerHand.filter((card) => {
+      return !selectedCards.includes(card);
+    });
     let newCard = deck.pop();
-
     if (newCard) {
       remainingCards.push(newCard);
     } else {
       console.log('Deck is empty, no more cards to draw.');
     }
-
     setActivePlayerHand(remainingCards);
     setHighlightedPairs([]);
     setHighlightedSequences([]);
   };
-
   return (
-    <div className='flex flex-col w-full bg-gray-200 items-center h-screen justify-center relative'>
-      <div
-        className='btn btn-primary bg-gray-300 border  border-gray-600 px-4 py-2 rounded cursor-pointer hover:bg-gray-400'
+    <div className='relative flex flex-col items-center justify-center w-full h-screen bg-gray-200'>
+      <button
         onClick={() => reloadPage()}
+        className='text-white btn btn-active btn-success'
       >
-        Deal Again
-      </div>
+        Deal Cards
+      </button>
+
       <h1>Player A's Cards</h1>
       <div style={{ display: 'flex', flexDirection: 'row' }}>
-        {activePlayerHand.map((card) => (
-          <Card
-            key={`${card.value}${card.suit}`}
-            card={card}
-            onClick={handleCardClick}
-            highlightType={
-              highlightedPairs.includes(card)
-                ? 'yellow'
-                : highlightedSequences.includes(card)
-                ? 'cyan'
-                : 'white'
-            }
-          />
-        ))}
+        {activePlayerHand.map(
+          (card) => (
+            console.log('card', card),
+            (
+              <Card
+                key={`${card.value}${card.suit}`}
+                card={card}
+                onClick={handleCardClick}
+                className={
+                  card == selectedCard
+                    ? 'bg-blue-200   shadow-md    -translate-y-2 border border-blue-300 '
+                    : 'bg-white'
+                }
+                // highlightType={}
+              />
+            )
+          )
+        )}
+        {console.log('seq', highlightedSequences)}
+      </div>
+      <div className='flex gap-3'>
+        {highlightedPairs.length > 0 && (
+          <div
+            className='p-1 bg-gray-300 border border-gray-400 rounded-lg'
+            style={{ display: 'flex', flexDirection: 'row' }}
+          >
+            {highlightedPairs.map(
+              (card) => (
+                console.log('card', card),
+                (
+                  <Card
+                    key={`${card.value}${card.suit}`}
+                    card={card}
+                    onClick={handleCardClick}
+                    highlightType={'white'}
+                  />
+                )
+              )
+            )}
+            {console.log('seq', highlightedSequences)}
+          </div>
+        )}
+        {highlightedSequences.length > 0 && (
+          <div
+            className='p-1 bg-gray-300 border border-gray-400 rounded-lg'
+            style={{ display: 'flex', flexDirection: 'row' }}
+          >
+            {highlightedSequences.map(
+              (card) => (
+                console.log('card', card),
+                (
+                  <Card
+                    key={`${card.value}${card.suit}`}
+                    card={card}
+                    onClick={handleCardClick}
+                    highlightType={'white'}
+                  />
+                )
+              )
+            )}
+          </div>
+        )}
       </div>
       {(highlightedPairs.length > 0 || highlightedSequences.length > 0) && (
-        <div>
+        <div className='flex gap-2 m-2 '>
           {highlightedPairs.length > 0 && (
-            <div
-              className='btn btn-primary bg-gray-300 border  border-gray-600 px-4 py-2 rounded cursor-pointer hover:bg-gray-400'
+            <button
               onClick={() => executeMove('pair')}
+              className='text-white btn btn-active btn-primary'
             >
               Play Pair
-            </div>
+            </button>
           )}
           {highlightedSequences.length > 0 && (
-            <div
-              className='btn btn-primary bg-gray-300 border  border-gray-600 px-4 py-2 rounded cursor-pointer hover:bg-gray-400'
+            <button
               onClick={() => executeMove('sequence')}
+              className='text-white btn btn-active btn-primary'
             >
               Play Sequence
-            </div>
+            </button>
           )}
         </div>
       )}
-      <div className='flex flex-col absolute bottom-1   '></div>
+      {/* <div className='absolute flex flex-col bottom-1 '></div>
       <p className='flex items-center text-center'>
         <strong>Instructions:&nbsp;</strong>Select a card if their exists a
         &nbsp;
         <strong>sequence</strong> &nbsp;it will be highlighted in Cyan
-        <span className='bg-cyan-300 h-4   w-4  rounded-full flex mx-1'></span>
+        <span className='flex w-4 h-4 mx-1 rounded-full bg-cyan-300'></span>
       </p>
       <p className='flex items-center text-center'>
         if their exists a &nbsp; <strong>Pair</strong> &nbsp;it will be
         highlighted in Yellow
-        <span className='bg-yellow-400 h-4   w-4  rounded-full flex mx-1'></span>
+        <span className='flex w-4 h-4 mx-1 bg-yellow-400 rounded-full'></span>
       </p>
-      <p>Created by Rushikesh</p>
+      <p>Created by Rushikesh</p> */}
     </div>
   );
 }
-
 export default App;
